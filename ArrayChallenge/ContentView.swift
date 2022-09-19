275a44ddb5c96fb4e79ff645307ef6f100a10d97
Esperanza <v.esperanzac113@gmail.com>
2022-09-19 14:20:34 -0700

    Upload the button code

--- ArrayChallenge/ContentView.swift
+++ ArrayChallenge/ContentView.swift
@@ -9,48 +9,101 @@ import SwiftUI
 
 struct Card {
   @State var trackNumber: Int = Int.random(in: 1...13)
-  @State var trackSuit = ["Spades", "Clubs", "Hearts", "Diamonds"].randomElement()
+  @State var trackSuit: String = ["Spades", "Clubs", "Hearts", "Diamonds"].randomElement()!
 }
 
 
 struct ContentView: View {
+  // Store instances of the structure
   @State var deck = [Card]()
-  @State var label: String = "Create an instance"
-    var body: some View {
-        VStack {
-            Text("Generated a duplicate card.")
-          Button {
-            let card1 = Card()
-            switch card1.trackNumber {
-            case 1:
-              label = "Generated a Ace of \(card1.trackSuit[0])."
-            case 11:
-              label = "Generated a Jack of \(card1.trackSuit[1])."
-            case 12:
-              label = "Generated a Queen of \(card1.trackSuit[2])."
-            case 13:
-              label = "Generated a King of \(card1.trackSuit[3])."
-            default:
-              label = "\(card1.trackNumber)"
-              return
-            }
-            
-          } label: {
-            Text(label)
-          }
-          Button {
-            <#code#>
-          } label: {
-            <#code#>
-          }
-
-          
-          }
-        .padding()
-        }
+  // it will show message depends on what we generate a card
+  @State var message: String = ""
+  // store every card we generate and check it has already existed or not
+  @State var cardArr: [String] = []
+  
+  var body: some View {
+    VStack {
+      Text(message)
+      Button {
+        generateCard()
         
+      } label: {
+        Text("Generate a card!")
+          .padding()
+          .font(.headline)
+          .foregroundColor(.white)
+          .background(Color.teal)
+          .cornerRadius(10)
+      }
+      Button {
+        selectCard()
+      } label: {
+        Text("Randomly selected a card")
+          .padding()
+          .font(.subheadline)
+          .bold()
+          .foregroundColor(.white)
+          .background(Color.yellow)
+          .cornerRadius(10)
+      }
+    }
+    .padding()
+  }
+  
+  func generateCard() {
+    // create one card object
+    let card = Card()
+    var newCard: String = ""
+    newCard = String(card.trackNumber) + card.trackSuit
+    
+    if cardArr.contains(newCard) {
+      message = "Generated duplicate card"
+    } else {
+      // Add value to cardArr
+      switch card.trackNumber {
+      case 1:
+        message = "Generated a Ace of \(card.trackSuit)."
+      case 11:
+        message = "Generated a Jack of \(card.trackSuit)."
+      case 12:
+        message = "Generated a Queen of \(card.trackSuit)."
+      case 13:
+        message = "Generated a King of \(card.trackSuit)."
+      default:
+        message = "Generated a \(card.trackNumber) of \(card.trackSuit)."
+      }
+      cardArr.append(newCard)
+      print(newCard)
+      print(cardArr)
+      deck.append(card)
+    }
+  }
+  
+  func selectCard() {
+    // check deck whether is Empty or not
+    if deck.isEmpty {
+      message = "No cards in the deck"
+    } else {
+      // pick up one randomly
+      let getCard = deck[Int.random(in: 0...deck.count - 1)]
+      
+      switch getCard.trackNumber {
+      case 1:
+        message = "Drew a Ace of \(getCard.trackSuit)"
+      case 11:
+        message = "Drew a Jack of \(getCard.trackSuit)"
+      case 12:
+        message = "Drew a Queen of \(getCard.trackSuit)"
+      case 13:
+        message = "Drew a King of \(getCard.trackSuit)"
+      default:
+        message = "Drew a \(getCard.trackNumber) of \(getCard.trackSuit)"
+      }
+    }
+  }
     
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
   
